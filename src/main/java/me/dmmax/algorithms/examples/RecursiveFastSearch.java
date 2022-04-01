c9e59505dda9cf6e68474ad64786ad385aae854a
Maksim Dimukhametov <dmmax.me@gmail.com>
2022-04-01 07:45:25 +0200

    Completed exercises for fast search chapter

---
+++ src/main/java/me/dmmax/algorithms/examples/RecursiveFastSearch.java
@@ -0,0 +1,78 @@
+package me.dmmax.algorithms.examples;
+
+import java.util.List;
+
+public class RecursiveFastSearch {
+
+    private RecursiveFastSearch() {
+        // Utility class
+    }
+
+    public static int sumLoop(List<Integer> integers) {
+        var total = 0;
+        for (var value : integers) {
+            total += value;
+        }
+        return total;
+    }
+
+    public static int sumRecursive(List<Integer> integers) {
+        if (integers.isEmpty()) {
+            return 0;
+        }
+        var firstValue = integers.get(0);
+        var sublist = integers.subList(1, integers.size());
+        if (sublist.isEmpty()) {
+            return firstValue;
+        }
+        return firstValue + sumRecursive(sublist);
+    }
+
+    public static int sizeOfListRecursive(List<Integer> integers) {
+        if (integers.isEmpty()) {
+            return 0;
+        }
+        int sizeOfList = 1;
+        var sublist = integers.subList(1, integers.size());
+        if (sublist.isEmpty()) {
+            return sizeOfList;
+        }
+        return sizeOfList + sizeOfListRecursive(sublist);
+    }
+
+    public static int findMaxNumberRecursive(List<Integer> integers) {
+        if (integers.isEmpty()) {
+            return Integer.MIN_VALUE;
+        }
+        var firstValue = integers.get(0);
+        var sublist = integers.subList(1, integers.size());
+        if (sublist.isEmpty()) {
+            return firstValue;
+        }
+        var nextValue = findMaxNumberRecursive(sublist);
+        return firstValue >= nextValue ? firstValue : nextValue;
+    }
+
+    public static int binarySearchRecursive(List<Integer> sortedIntegers, int valueToFind) {
+        var low = 0;
+        var high = sortedIntegers.size() - 1;
+        return binarySearchRecursive(sortedIntegers, valueToFind, low, high);
+    }
+
+    private static int binarySearchRecursive(List<Integer> sortedIntegers, int valueToFind, int low, int high) {
+        if (low <= high) {
+            var mid = (low + high) / 2;
+            var value = sortedIntegers.get(mid);
+            if (value == valueToFind) {
+                return mid;
+            }
+            if (value > valueToFind) {
+                high = mid - 1;
+                return binarySearchRecursive(sortedIntegers, valueToFind, low, high);
+            }
+            low = mid + 1;
+            return binarySearchRecursive(sortedIntegers, valueToFind, low, high);
+        }
+        return -1;
+    }
+}
---
+++ src/test/java/me/dmmax/algorithms/examples/RecursiveFastSearchTest.java
@@ -0,0 +1,69 @@
+package me.dmmax.algorithms.examples;
+
+import me.dmmax.algorithms.util.Collections;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.List;
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class RecursiveFastSearchTest {
+
+    private static final List<Integer> TEST_LIST = List.of(1, 2, 3, 4, 5);
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void testSum(int sizeOfList) {
+        var generatedList = Collections.generateRandomList(sizeOfList, 10, 30);
+        var sumLoop = RecursiveFastSearch.sumLoop(generatedList);
+        var sumRecursive = RecursiveFastSearch.sumRecursive(generatedList);
+        assertThat(sumLoop).isEqualTo(sumRecursive);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void testSizeOfListRecursive(int sizeOfList) {
+        var generatedList = Collections.generateRandomList(sizeOfList, 10, 30);
+        assertThat(RecursiveFastSearch.sizeOfListRecursive(generatedList)).isEqualTo(sizeOfList);
+    }
+
+    @ParameterizedTest
+    @MethodSource("testCases")
+    void testFindMaxNumberRecursive(int sizeOfList) {
+        var generatedList = Collections.generateRandomList(sizeOfList, 10, 30);
+        var maxValue = generatedList.stream().max(Integer::compare).orElse(Integer.MIN_VALUE);
+        assertThat(RecursiveFastSearch.findMaxNumberRecursive(generatedList)).isEqualTo(maxValue);
+    }
+
+    @ParameterizedTest
+    @MethodSource("binarySearchTestCases")
+    void testBinarySearchRecursive(List<Integer> sortedIntegers, int valueToFind, int expectedValue) {
+        assertThat(RecursiveFastSearch.binarySearchRecursive(sortedIntegers, valueToFind)).isEqualTo(expectedValue);
+    }
+
+    private static Stream<Arguments> testCases() {
+        return Stream.of(
+                Arguments.of(0),
+                Arguments.of(1),
+                Arguments.of(2),
+                Arguments.of(5),
+                Arguments.of(10),
+                Arguments.of(50),
+                Arguments.of(100));
+    }
+
+    private static Stream<Arguments> binarySearchTestCases() {
+        return Stream.of(
+                Arguments.of(TEST_LIST, 1, 0),
+                Arguments.of(TEST_LIST, 2, 1),
+                Arguments.of(TEST_LIST, 3, 2),
+                Arguments.of(TEST_LIST, 4, 3),
+                Arguments.of(TEST_LIST, 5, 4),
+                Arguments.of(TEST_LIST, 6, -1),
+                Arguments.of(List.of(), 7, -1));
+    }
+}
